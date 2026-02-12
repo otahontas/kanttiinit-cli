483bc5feee64697285c7a36b173bee844784d588
Otto Ahoniemi <otto@ottoahoniemi.fi>
2026-02-12 12:49:01 +0200

    feat: add flake.nix and split release workflow for binary uploads

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "CLI for browsing Helsinki area student restaurant menus from Kanttiinit.fi";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.rustPlatform.buildRustPackage {
+            pname = "kanttiinit";
+            version = "0.1.0";
+            src = ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+            meta = {
+              description = "CLI for browsing Helsinki area student restaurant menus from Kanttiinit.fi";
+              homepage = "https://github.com/otahontas/kanttiinit-cli";
+              license = pkgs.lib.licenses.mit;
+              mainProgram = "kanttiinit";
+            };
+          };
+        }
+      );
+    };
+}
