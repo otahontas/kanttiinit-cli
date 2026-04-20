a00314e15d0a4b127c5304c6592a0a8393c6d5ec
Otto Ahoniemi <otto@ottoahoniemi.fi>
2026-04-20 09:34:18 +0300

    chore: bump version to 0.3.6

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
         {
           default = pkgs.rustPlatform.buildRustPackage {
             pname = "kanttiinit";
-            version = "0.3.5";
+            version = "0.3.6";
             src = ./.;
             cargoLock.lockFile = ./Cargo.lock;
             meta = {
